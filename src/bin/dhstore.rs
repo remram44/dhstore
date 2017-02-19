d9c3e2a1c0a4cb0bf94cc6604ce49099ab5ad290
Remi Rampin <remirampin@gmail.com>
2017-02-19 00:20:13 -0500

    Add dhstore::open() for standard directories

--- src/bin/dhstore.rs
+++ src/bin/dhstore.rs
@@ -1,13 +1,5 @@
 extern crate dhstore;
 
 fn main() {
-    // Create a file blob storage, storing blobs as single files
-    let storage = dhstore::FileBlobStorage::open("store/blobs");
-
-    // Create a memory index, that stores all the objects in memory, and
-    // has to load all of them everytime from simple files
-    let index = dhstore::MemoryIndex::open("store/objects");
-
-    // Create the Store object
-    let store = dhstore::Store::open(storage, index);
+    let store = dhstore::open("store");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -18,10 +18,34 @@ pub struct Store<S: BlobStorage, I: ObjectIndex> {
 }
 
 impl<S: BlobStorage, I: ObjectIndex> Store<S, I> {
-    pub fn open(storage: S, index: I) -> Store<S, I> {
+    pub fn new(storage: S, index: I) -> Store<S, I> {
         Store {
             storage: storage,
             index: index,
         }
     }
+}
+
+pub fn open<P: AsRef<std::path::Path>>(path: P) ->
+    Store<FileBlobStorage, MemoryIndex>
+{
+    let path = path.as_ref();
+
+    // Create a file blob storage, storing blobs as single files
+    let storage = {
+        let mut blobs = path.to_path_buf();
+        blobs.push("blobs");
+        FileBlobStorage::open(blobs)
+    };
+
+    // Create a memory index, that stores all the objects in memory, and
+    // has to load all of them everytime from simple files
+    let index = {
+        let mut objects = path.to_path_buf();
+        objects.push("objects");
+        MemoryIndex::open(objects)
+    };
+
+    // Create the Store object
+    Store::new(storage, index)
 }
